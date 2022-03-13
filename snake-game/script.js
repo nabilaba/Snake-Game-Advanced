b7616d691d963ac3452f5bf2408d4af08ed23b69
nabilaba <nabilazizbimaanggita@gmail.com>
2022-03-13 16:42:02 +0700

    Android fix support control

--- snake-game/script.js
+++ snake-game/script.js
@@ -454,6 +454,53 @@ document.addEventListener("keydown", function (event) {
     }
 })
 
+// Android Swipe Control Support
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||
+         evt.originalEvent.touches;
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 ) {
+            turn(snake1, DIRECTION.LEFT); 
+        } else {
+            turn(snake1, DIRECTION.RIGHT);
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            turn(snake1, DIRECTION.UP);
+        } else { 
+            turn(snake1, DIRECTION.DOWN);
+        }                                                                 
+    }
+    
+    xDown = null;
+    yDown = null;                                             
+};
+
 function initGame() {
     move(snake1);
 }
